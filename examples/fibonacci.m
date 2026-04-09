df3da6d01ee1fe25b2d00b4dd4b35899984e380a
Grzegorz Grudzień <ggrudzien@student.agh.edu.pl>
2026-04-09 18:02:15 +0200

    some fixes in structure

---
+++ examples/fibonacci.m
@@ -0,0 +1,8 @@
+a = 0;
+b = 1;
+while (b < 1000) {
+    print b;
+    b += a;
+    a = b - a;
+}
+u=z;
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from Interpreter import Interpreter
 
 if __name__ == "__main__":
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "../examples/fibonacci.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "../examples/primes.m"
         with open(filename, "r") as file:
             text = file.read()
     except IOError:
